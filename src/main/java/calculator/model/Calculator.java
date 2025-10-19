ae279a71c74ed669f89e84ebb2b506555e098997
sangwookkhu <ksw090711@gmail.com>
2025-10-19 20:58:51 +0900

    [프리코스 1주차]feat: 덧셈 계산 기능 구현

---
+++ src/main/java/calculator/model/Calculator.java
@@ -0,0 +1,11 @@
+package calculator.model;
+
+public class Calculator {
+    public int sum(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
+}
